c7128ff403b08c5a7e45c0c5b1c22cab89e60023
Raredemon <raredemon@gmail.com>
2019-07-26 15:56:20 +0300

    get_vlans is done

---
+++ BDCOM/xPON/get_vlans.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## BDCOM.3310C.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2019-2019 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+__author__ = 'Raredemon'
+
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVlans
+import re
+
+
+class Script(NOCScript):
+    name = "BDCOM.xPON.get_vlans"
+    implements = [IGetVlans]
+    rx_vlan = re.compile(
+        r"^(?P<vlanid>\d+)\s+Static\s+(?P<vlanname>VLAN\d+)",
+        re.MULTILINE)
+
+    def execute(self):
+        r = []
+        for match in self.rx_vlan.finditer(self.cli("show vlan")):
+            r += [{
+                "vlan_id": int(match.group('vlanid')),
+                "name": match.group('vlanname')
+            }]
+        return r
